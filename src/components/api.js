9c8db5c06a8115a027a8212c42c1da98f00cc2e4
Mustafa Hamraz <mustafahamraz@h-155-190.A197.priv.bahnhof.se>
2021-05-14 11:38:45 +0200

    the app has been deployed

--- src/components/api.js
+++ src/components/api.js
@@ -1,6 +1,13 @@
-export const API_LOGIN = 'http://localhost:5000/login';
-export const API_CREATE_ACCOUNT = 'http://localhost:5000/create_account';
-export const API_GET_ITEMS = 'http://localhost:5000/get_items';
-export const API_EDIT_ITEM = 'http://localhost:5000/edit_item';
-export const API_NEW_ITEM = 'http://localhost:5000/new_item';
-export const API_DELETE_ITEM = 'http://localhost:5000/delete_item';
+// export const API_LOGIN = 'http://localhost:5000/login';
+// export const API_CREATE_ACCOUNT = 'http://localhost:5000/create_account';
+// export const API_GET_ITEMS = 'http://localhost:5000/get_items';
+// export const API_EDIT_ITEM = 'http://localhost:5000/edit_item';
+// export const API_NEW_ITEM = 'http://localhost:5000/new_item';
+// export const API_DELETE_ITEM = 'http://localhost:5000/delete_item';
+
+export const API_LOGIN = 'https://u11-shoping-list.herokuapp.com/login';
+export const API_CREATE_ACCOUNT = 'https://u11-shoping-list.herokuapp.com/create_account';
+export const API_GET_ITEMS = 'https://u11-shoping-list.herokuapp.com/get_items';
+export const API_EDIT_ITEM = 'https://u11-shoping-list.herokuapp.com/edit_item';
+export const API_NEW_ITEM = 'https://u11-shoping-list.herokuapp.com/new_item';
+export const API_DELETE_ITEM = 'https://u11-shoping-list.herokuapp.com/delete_item';
